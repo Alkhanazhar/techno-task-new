c6cb463f3e0073d8274c820faf735c10b6148f85
alkhanazhar <khan.azhar.saf@gmail.com>
2025-10-01 18:28:13 +0530

    rechange book a free demo

--- components/hero/BookDemo.jsx
+++ components/hero/BookDemo.jsx
@@ -8,7 +8,7 @@ const BookDemo = () => {
           className="w-full h-full object-cover opacity-20"
         />
       </div>
-      <div className="max-w-6xl mx-auto border border-white/10 bg-gradient-to-br  to-[#482552] from-[#23062C] rounded-2xl text-center px-6 md:py-24 py-8 shadow-lg relative overflow-hidden">
+      <div className="max-w-6xl mx-auto border border-white/10 bg-gradient-to-br  to-[#371d3e] from-[#3F2AB2] rounded-2xl text-center px-6 md:py-24 py-8 shadow-lg relative overflow-hidden">
         {/* Gradient Lighting Effects */}
         <div className="absolute top-0 left-0 w-60 h-60 bg-[#3F2AB2] blur-[120px] opacity-30 rounded-full -z-10"></div>
         <div className="absolute bottom-0 right-0 w-60 h-60 bg-purple-700 blur-[120px] opacity-30 rounded-full -z-10"></div>
