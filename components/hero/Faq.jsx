7b364af3c200552cb8ac15c5864c7638fb7447e4
alkhanazhar <khan.azhar.saf@gmail.com>
2025-10-08 17:28:09 +0530

    Footer fix

--- components/hero/Faq.jsx
+++ components/hero/Faq.jsx
@@ -38,7 +38,7 @@ const Faq = () => {
   };
 
   return (
-    <section className=" md:py-24 -mt-24 py-12  relative px-4 md:px-8">
+    <section className=" md:py-24 sm:-mt-24 py-12  relative px-4 md:px-8">
       {/* <div className="absolute inset-0 z-0">
         <img
           src="/bgPattern.png"
